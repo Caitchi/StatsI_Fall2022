1551be73d1de0f82dbbe0460f859cf593975be70
Caitchi <cooneyc4@tcd.ie>
2022-10-16 12:35:16 +0100

    ygtgyg

--- problemSets/PS02/My_answers/PS02_Response.R
+++ problemSets/PS02/My_answers/PS02_Response.R
@@ -42,8 +42,8 @@ setwd("~/Desktop/ASDS/StatsI_Fall2022/problemSets/PS02/My_answers/")
 # H0: The variables are statistically independent 
 # H1: The variables are statistically dependent 
 
-
 # Create a matrix of our data and name the rows and columns
+
 driver <- matrix(c(14,6,7,7,7,1),ncol=3,byrow=TRUE)  
 colnames(driver) <- c("Not Stopped","Bribe Requested","Stopped/Given Warning")
 rownames(driver) <- c("Upper Class","Lower Class")
@@ -54,19 +54,21 @@ barplot(driver,legend=TRUE,beside=TRUE,main='Drivers stopped by police')
 
 
 margin.table(driver)
-
 margin.table(driver,1)
-
 margin.table(driver,2)
+
 
 # Calculate the table of expected values by multiplying the vectors of the 
 # margins and dividing by the total number of observations (The “t” function 
 # takes the transpose of the array.)
+
 expected <- as.array(margin.table(driver,1)) %*% t(as.array(margin.table(driver,2))) / margin.table(driver)
 expected
 
+
 #  We need the square of the difference between the two tables divided by the 
 # expected values. The sum of all these values is the Chi-squared statistic:
+
 chi <- sum((expected - as.array(driver))^2/expected)
 chi
 
@@ -83,56 +85,66 @@ summary(driver)
 # b) 
 ###
 
-# Calculate the degrees of freedom
-df <- (ncol(driver) - 1)(nrows(driver) - 1) # Degrees of Freedom = (no. rows - 1)(no. columns -1)
-df
+
+# Calculate the degrees of freedom = (no. columns - 1)(no. rows -1)
+
+df <- (ncol(driver) - 1)*(nrow(driver) - 1) 
+df                 
 
 # Find the p-value
-p_value <- 1-pchisq(chi,df=2)
+
+p_value <- 1-pchisq(chi, df=2)
 p_value
 
+
 # If a = 0.1, then a p-value of 0.1502306 (which is greater than 0.10) 
-# is considered not significant as p > 0.10, so we fail to reject the null hypothesis.
+# is considered not significant as p > 0.10, so we fail to reject the null hypothesis. 
+
+# Check if this is valid -> one tailed test and not two tailed so don't
+# need to divide by 2
+
 
 ### 
 # c) 
 ###
 
-# Use the str() function to see the standardised residuals and then create a 
-# table by calling stdres of the chisq.test() function:
 
-chi2 <- chisq.test(driver, correct = FALSE)
 
+# We calculate the standard residuals by subtracting the #expected values 
+# from the observation and dividing by the #square root of the expected values 
+
+std_residuals <- (driver - expected) / sqrt(expected)
+std_residuals
+
+
+# check if R agrees 
 str(chi2)
-chi2$stdres
+chi2$residuals
 
 
 ### 
 # d) 
 ###
 
-# The standardized residual is a measure of the strength of the difference 
-# between observed and expected values. It’s a measure of how significant your 
-# cells are to the chi-square value. When you compare the cells, the 
-# standardized residual makes it easy to see which cells are contributing the 
-# most to the value, and which are contributing the least. 
 
-# (If the value of standardized residual is lower than -2 it means that the cell 
-# contains fewer observations that it was expected (the case of variables 
-# independence). If the value of standardized residual is higher than 2 it means
-# that the cell contains more observations that it was expected.)
+# In this dataset, the standardized residuals indicate that more lower class 
+# drivers were approached for a bribe than expected (std res slightly greater than
+# 1) , however fewer lower class drivers were stopped and given a warning than 
+# expected. A comparable number of upper and lower class drivers were not 
+# stopped at all when then numbers in each class are accounted for (std res 
+# close to 0). 
 
-# Positive residuals in cells specify a positive association between the 
-# corresponding row and column variables.
-# Negative residuals imply negative association between the corresponding row 
-# and column variables.
+# Fewer bribes than expected were solicited from upper class individuals,  
+# however more than expected were stopped and given a warning. However, the 
+# absolute value of the standardized residuals in these cells were less than 1 
+# and so not considered significant. 
 
-# In this case, upper class drivers were more likely not to be stopped, while 
-# lower class drivers were less likely to not be stopped.
-# Upper class drivers, if stopped, were less likely to be asked for a bribe, 
-# while lower class drivers were more likely to be asked for a bribe. 
-# Upper class drivers, if stopped, were more likely to be given a warning, 
-# and lower class drivers were less likely to be given a warning. 
+# Overall the most significant contributor to the chi squared test were lower 
+# class drivers being asked for bribes and lower class drivers stopped and given 
+# a warning. However, the relatively small values for the standardized residuals
+# support our rejection of the alternate hypothesis that these variables are 
+# dependent and do support the adoption of the null hypothesis. 
+
 
 
 #####################
